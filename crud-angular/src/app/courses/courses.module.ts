d34a91d789cb9831de7d7a81a121c3f204b1bdcb
Bruno Marques <brunoaffonsomarques@gmail.com>
2022-08-08 15:51:32 -0300

    Version 1.0
    Class 13 Loiane youtube

---
+++ crud-angular/src/app/courses/courses.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CoursesRoutingModule } from './courses-routing.module';
+import { CoursesComponent } from './courses/courses.component';
+import { AppMaterialModule } from '../shared/app-material/app-material.module';
+import { HttpClientModule } from '@angular/common/http';
+import { SharedModule } from '../shared/shared.module';
+
+@NgModule({
+  declarations: [
+    CoursesComponent
+  ],
+  imports: [
+    CommonModule,
+    CoursesRoutingModule,
+    AppMaterialModule,
+    HttpClientModule,
+    SharedModule
+  ]  
+})
+export class CoursesModule { }
